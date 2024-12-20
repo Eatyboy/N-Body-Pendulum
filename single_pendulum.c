3c9eb0e6ee9a3f6d641a051f9432acab3ef3719f
Ethan Carter <eatyboy32@gmail.com>
2024-12-19 19:59:12 -0800

    fixed RK4, added speedup, and added energy info

--- single_pendulum.c
+++ single_pendulum.c
@@ -3,25 +3,27 @@
 #include "include/raylib.h"
 #include "include/raymath.h"
 
-#define GRAVITY (-150.0f) // this just worked best
+#define GRAVITY (200.0f) // this just worked best
 #define RADIUS 32
 #define MAX_SPEED 16.0f
 #define MIN_SPEED 0.0625f
 #define ONE_SIXTH 0.16666667f
 
-#define RK4 1
+#define RK4 1 // 1: use Runge-Kutta 4, 0: use forward Euler
 
 typedef struct Body {
 	float mass;
 	float length;
 	float theta;
-	float thetaDot;
+	float omega;
 } Body;
 
-void render(Body body, Vector2 origin, float speedup);
+void render(Body body, Vector2 origin);
 void solve(Body *body, float dt);
 Vector2 getPos(Body body);
 float func(float theta, float length); // f in dx/dt = f(x, t) in numerical integration
+float getEnergy(Body body);
+void drawCenteredText(const char *text, int parentX, int parentWidth, int posY, int fontSize, Color color);
 
 int main(void) {
 	const Vector2 screenSize = {1280, 720};
@@ -33,9 +35,15 @@ int main(void) {
 
 	float speedup = 1;
 
-	Vector2 origin = (Vector2){screenSize.x / 2, screenSize.y / 4};
+	Vector2 origin = (Vector2){screenSize.x / 2, screenSize.y / 4 + 100};
 
-	Body pendulum = (Body){10, 300, 0.4f * PI};
+	Body pendulum = (Body){10, 300, 0.4f * PI, 0};
+
+	/*
+	FILE *pendulumData;
+	fopen_s(&pendulumData, "output.txt", "w");
+	*/
+	float initialEnergy = getEnergy(pendulum);
 
 	while (!WindowShouldClose()) {
 		float dt = GetFrameTime();
@@ -47,66 +55,72 @@ int main(void) {
 			speedup *= 0.5f;
 		}
 
-		solve(&pendulum, dt * speedup);
-		render(pendulum, origin, speedup);
+		// Numerically integrate to solve the system according to the
+		// differential equation given by the Euler-Lagrange equation
+		if (speedup > 1.0f - EPSILON) {
+			int steps = (int)(speedup + EPSILON);
+			for (int i = 0; i < steps; ++i) {
+				solve(&pendulum, dt);
+			}
+		} else {
+			solve(&pendulum, dt * speedup);
+		}
+		float energy = getEnergy(pendulum);
+
+		BeginDrawing();
+
+		ClearBackground(BLACK);
+
+		// Draw the system
+		render(pendulum, origin);
+
+		// Speedup text
+		const char *speedupText = (speedup >= 1 - EPSILON)
+			? TextFormat("Speed: %dX", (int)speedup)
+			: TextFormat("Speed: 1/%dX", (int)(1.0f / speedup));
+		drawCenteredText(speedupText, 0, GetScreenWidth(), 50, 36, WHITE);
+
+		//Energy text;
+		DrawText(TextFormat("Initial energy: %f", initialEnergy), 20, 20, 24, WHITE);
+		DrawText(TextFormat("Current energy: %f", energy), 20, 60, 24, WHITE);
+		DrawText(TextFormat("Energy difference: %f", energy - initialEnergy), 20, 100, 24, WHITE);
+
+		EndDrawing();
 	}
+
+	//fclose(pendulumData);
 
 	CloseWindow();
 
 	return 0;
 }
 
-void render(Body body, Vector2 origin, float speedup) {
-	BeginDrawing();
-
-	ClearBackground(BLACK);
-
-	// Pendulum
-	{
-		Vector2 pos = Vector2Add(origin, getPos(body));
-		DrawLineV(origin, pos, WHITE);
-		DrawCircleV(origin, 0.3f * RADIUS, RED);
-		DrawCircleV(pos, RADIUS, BLUE);
-	}
-
-	// Speedup text
-	{
-
-		const char *text = (speedup >= 1 - EPSILON)
-			? TextFormat("Speed: %dX", (int)speedup)
-			: TextFormat("Speed: 1/%dX", (int)(1.0f / speedup));
-		float textWidth = MeasureText(text, 36);
-		DrawText(text, 0.5f * (GetScreenWidth() - textWidth), 50, 36, WHITE);
-	}
-
-	EndDrawing();
+void render(Body body, Vector2 origin) {
+	Vector2 pos = Vector2Add(origin, getPos(body));
+	DrawLineV(origin, pos, WHITE);
+	DrawCircleV(origin, 0.3f * RADIUS, RED);
+	DrawCircleV(pos, RADIUS, BLUE);
 }
 
 void solve(Body *body, float dt) {
-	float omega = PI / 4;
-
 	#if RK4
 
-	float f1, f2, f3, f4;
+	float omegaDot1 = func(body->theta, body->length);
+	float thetaDot1 = body->omega;
+	float omegaDot2 = func(body->theta + 0.5f * dt * thetaDot1, body->length);
+	float thetaDot2 = body->omega + 0.5f * dt * omegaDot1;
+	float omegaDot3 = func(body->theta + 0.5f * dt * thetaDot2, body->length);
+	float thetaDot3 = body->omega + 0.5f * dt * omegaDot2;
+	float omegaDot4 = func(body->theta + dt * thetaDot3, body->length);
+	float thetaDot4 = body->omega + dt * omegaDot3;
 
-	f1 = func(body->theta, body->length);
-	f2 = func(body->theta + 0.5f * dt * f1, body->length);
-	f3 = func(body->theta + 0.5f * dt * f2, body->length);
-	f4 = func(body->theta + dt * f3, body->length);
-
-	body->thetaDot += ONE_SIXTH * (f1 + 2*f2 + 2*f3 + f4);
-
-	f1 = body->thetaDot;
-	f2 = body->thetaDot + 0.5f * dt * f1;
-	f3 = body->thetaDot + 0.5f * dt * f2;
-	f4 = body->thetaDot + dt * f3;
-
-	body->theta += ONE_SIXTH * dt * (f1 + 2*f2 + 2*f3 + f4);
+	body->omega += ONE_SIXTH * dt * (omegaDot1 + 2*omegaDot2 + 2*omegaDot3 + omegaDot4);
+	body->theta += ONE_SIXTH * dt * (thetaDot1 + 2*thetaDot2 + 2*thetaDot3 + thetaDot4);
 
 	#else
 	
-	body->theta += dt * body->thetaDot;
-	body->thetaDot += dt * func(body->theta, body->length);
+	body->theta += dt * body->omega;
+	body->omega += dt * func(body->theta, body->length);
 
 	#endif
 
@@ -121,5 +135,18 @@ Vector2 getPos(Body body) {
 }
 
 float func(float theta, float length) {
-	return GRAVITY / length * sinf(theta);
+	return - (GRAVITY / length) * sinf(theta);
+}
+
+float getEnergy(Body body) {
+	// E = 0.5mv^2 + mgh
+	float kinetic = 0.5f * body.mass * body.length * body.length * body.omega * body.omega;
+	float potential = body.mass * GRAVITY * body.length * (1 - cosf(body.theta));
+	return kinetic + potential;
+}
+
+void drawCenteredText(const char *text, int parentX, int parentWidth, int posY, int fontSize, Color color) {
+	float textWidth = MeasureText(text, 36);
+	int posX = parentX + 0.5f * (parentWidth - textWidth);
+	DrawText(text, posX, posY, fontSize, color);
 }
